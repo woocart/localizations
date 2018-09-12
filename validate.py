8914ecc537783fa095a805d531d3dcd10af6cb96
Nejc Zupan <nejc.zupan@gmail.com>
2018-09-12 14:56:08 +0100

    fix a bunch of bugs found by the validation script

--- validate.py
+++ validate.py
@@ -1,6 +1,27 @@
 """Validate that localization files conform to defined schemas."""
 
 from schema import YamlSchema
+from strictyaml import StrictYAMLError
+
+import os
+
+# validate country files
+for country in os.listdir("Countries"):
+    try:
+        print(f'Validating {country}/local.yaml" ...')
+        YamlSchema.load("Countries", f"{country}/local.yaml", YamlSchema.localization)
+    except StrictYAMLError as e:
+        print(e)
+        print(
+            f'!!! Error parsing "{country}/local.yaml". Look above for the reason. !!!'
+        )
+        exit(1)
 
 # validate template.yaml
-schema = YamlSchema.load("./", "template.yaml", YamlSchema.localization)
+try:
+    print('Validating "template.yaml" ...')
+    YamlSchema.load("./", "template.yaml", YamlSchema.localization)
+except StrictYAMLError as e:
+    print(e)
+    print('!!! Error parsing "template.yaml". Look above for the reason. !!!')
+    exit(1)
