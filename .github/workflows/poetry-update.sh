143622bcebcb07c93c69cb5bc9dfb76f814a900b
Arijit Basu <sayanarijit@gmail.com>
2020-07-23 12:56:02 +0530

    Add auto package updater
    Ref: https://github.com/niteoweb/operations/issues/1232

---
+++ .github/workflows/poetry-update.sh
@@ -0,0 +1,23 @@
+set -xeu
+
+# Fetch unshallow update
+git fetch --unshallow
+
+# Update the python packages
+poetry update
+
+# Only continue if there are any changes
+if ! git diff-index --quiet HEAD; then
+  today=$(date -I)
+  # Commit the new changes and push them to the repository so a PR can be opened
+  git checkout -b "poetry-update-$today"
+  git config user.name "poetry updater"
+  git add poetry.lock
+  git commit -m "poetry update $today"
+  git push -u origin "poetry-update-$today"
+  curl -X POST -L https://api.github.com/repos/${GITHUB_REPOSITORY:?}/pulls \
+	-H "Accept: application/vnd.github.v3+json" \
+	-H "Authorization: Bearer ${GITHUB_TOKEN:?}" \
+  	-d '{"title": "Update Python Packages", "head": "poetry-update-'$today'", "base": "master"}' \
+	--fail
+fi
